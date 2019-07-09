8fbf237583fed01ebe8eaf715e2781a2d3857e43
guanzhiyong <guanzhiyong@jiudaotech.com>
2019-07-09 17:33:39 +0800

    修复icon显示不全的问题

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -60,8 +60,8 @@ export default {
 
 <style scoped>
 .svg-icon {
-  width: 1em;
-  height: 1em;
+  width: 1.2em;
+  height: 1.2em;
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
