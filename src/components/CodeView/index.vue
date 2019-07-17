8d12977e3c6aacd8890d61645b3bf13702d5141b
yingxiang <a357375084@gmail.com>
2019-07-17 22:49:02 +0800

    指定file-saver xlsx的版本 增加codemirror依赖 增加代码片段组件 支持html与js高亮

---
+++ src/components/CodeView/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="json-editor">
+    <textarea ref="textarea" />
+  </div>
+</template>
+
+<script>
+import CodeMirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/night.css'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/mode/javascript/javascript'
+
+export default {
+  name: 'CodeView',
+  /* eslint-disable vue/require-prop-types */
+  props: {
+    value: {
+      type: String
+    },
+    mode: {
+      type: String,
+      default: 'text/html'
+    }
+  },
+  data() {
+    return {
+      codeView: false
+    }
+  },
+  mounted() {
+    this.codeView = CodeMirror.fromTextArea(this.$refs.textarea, {
+      mode: this.mode,
+      readOnly: true,
+      theme: 'night'
+    })
+
+    this.codeView.setValue(this.value)
+  }
+}
+</script>
+
+<style scoped>
+.json-editor{
+  height: 100%;
+  position: relative;
+}
+.json-editor >>> .CodeMirror {
+  height: auto;
+  min-height: 300px;
+}
+.json-editor >>> .CodeMirror-scroll{
+  min-height: 300px;
+}
+.json-editor >>> .cm-s-rubyblue span.cm-string {
+  color: #F08047;
+}
+</style>
